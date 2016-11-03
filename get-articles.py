56db0f8134b662a54fe3b67f603b5b7e5413c396
andrewdbass <andybass12@gmail.com>
2016-11-02 21:55:33 -0600

    updated article sources

--- get-articles.py
+++ get-articles.py
@@ -1,21 +1,26 @@
 from urllib.request import Request, urlopen
 import json
-
-req = Request('https://medium.com/_/api/stream/?format=json&to=1476756061722&source=1', headers={'User-Agent': 'Mozilla/5.0'})
-data = urlopen(req).read().decode('utf-8')
-start = data.index('</x>') + 4
-data = data[start:]
-data = json.loads(data)
-
 arr = []
-articlesArray = data['payload']['references']['Post']
 
-for element in articlesArray:
-    obj = {};
-    obj['title'] = articlesArray[element]['title']
-    obj['durration'] = articlesArray[element]['virtuals']['readingTime']
-    obj['id'] = articlesArray[element]['id']
-    if 'previewImage' in articlesArray[element]['virtuals']:
-        obj['image'] = articlesArray[element]['virtuals']['previewImage']["imageId"]
-    arr.append(obj)
-print(arr)
+def get_data(req):
+    req = Request(req, headers={'User-Agent': 'Mozilla/5.0'})
+    data = urlopen(req).read().decode('utf-8')
+    start = data.index('</x>') + 4
+    data = data[start:]
+    data = json.loads(data)
+
+    articlesArray = data['payload']['value']
+    for element in articlesArray:
+        if element['title'] not in arr:
+            arr.append(element['title'])
+
+tags = ["business", "startups", "sports", "politics", "technology",
+        "humor", "entertainment", 'life', 'travel', 'user-experience',
+        'design', 'inspiration', 'inovation', 'science', 'history',
+        'adventure','creativity', 'self-improvement', 'photography',
+        'writing', 'ideas','life-lessons']
+
+for tag in tags:
+    get_data("https://medium.com/_/api/tags/"+tag+"/posts?format=json")
+
+print(len(arr))
