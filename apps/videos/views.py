18b9d248f1675f5b717d8b7a1f925abf1900f69b
andrewdbass <andybass12@gmail.com>
2016-11-24 19:17:04 -0700

    fixed filter error

--- apps/videos/views.py
+++ apps/videos/views.py
@@ -12,9 +12,9 @@ class VideoViewSet(viewsets.ModelViewSet):
         by filtering against a `username` query parameter in the URL.
         """
         queryset = Video.objects.all()
-        dur = self.request.query_params.get('`duration`', None)
+        dur = self.request.query_params.get('duration', None)
         tags = self.request.query_params.getlist('tags', None)
-
+        print(dur)
         if tags is not None:
             queryset = queryset.filter(tags__in=tags).distinct()
         if dur is not None:
