b4d83c6901358e79815f6c74365ac350a7712799
andrewdbass <andybass12@gmail.com>
2016-12-05 22:52:55 -0700

    fixed podcasts view'

--- apps/podcasts/views.py
+++ apps/podcasts/views.py
@@ -13,6 +13,13 @@ class PodcastViewSet(viewsets.ModelViewSet):
         """
         queryset = Podcast.objects.all()
         dur = self.request.query_params.get('duration', None)
+        tags = self.request.query_params.getlist('tags', None)
+        article_id = self.request.query_params.get('id', None)
+        print(dur)
+        if len(tags) != 0:
+            queryset = queryset.filter(tags__in=tags).distinct()
         if dur is not None:
             queryset = queryset.filter(duration__lte=dur).order_by('-duration','-id')
+        if article_id is not None:
+            queryset = queryset.filter(id=article_id)
         return queryset
