a6586b6be59f874bf6e5668b7d37d8a4af47088c
andrewdbass <andybass12@gmail.com>
2017-01-05 17:24:47 -0700

    fixed video script

--- apps/videos/management/commands/get_videos.py
+++ apps/videos/management/commands/get_videos.py
@@ -16,7 +16,7 @@ from datetime import datetime, timedelta
 
 class Command(BaseCommand):
 
-    def read_playlist(self, playlist, pageToken, vids, tags):
+    def read_playlist(self, playlist, pageToken, tags):
          DEVELOPER_KEY = settings.GOOGLE_API_KEY
          YOUTUBE_API_SERVICE_NAME = "youtube"
          YOUTUBE_API_VERSION = "v3"
@@ -35,24 +35,47 @@ class Command(BaseCommand):
            list_of_video_ids.append(item['snippet']['resourceId']['videoId'])
 
          list_of_video_ids = ",".join(list_of_video_ids)
-         print(list_of_video_ids)
+        #  print(list_of_video_ids)
          search = youtube.videos().list(
            id=list_of_video_ids,
            part="id,snippet,contentDetails",
            maxResults="50"
          ).execute()
-         items = []
+
          for item in search['items']:
-             print(item)
-             item['tags'] = tags
-             items.append(item)
-         vids.extend(items)
+             title = item['snippet']['title']
+             link = "https://www.youtube.com/embed/"+item['id']+"/?autoplay=1&fs=1"
+             if 'standard' in item['snippet']['thumbnails']:
+                 image = item['snippet']['thumbnails']['standard']['url']
+             else:
+                 image = item['snippet']['thumbnails']['default']['url']
+             source = "Youtube"
+             duration = isodate.parse_duration(item['contentDetails']['duration']).total_seconds()/60 +1
+             published = item['snippet']['publishedAt']
+             published = published[:published.find("T")]
+             # 2016-11-22T00:29:37.000Z
+             date_object = datetime.strptime(published, '%Y-%m-%d')
+            #  print(date_object)
+            #  print(title + " " + link + " " + image + " " + source + " " + str(duration))
+             all_videos = Video.objects.all()
+             if not all_videos.filter(title=title).exists() and date_object>=datetime.now()-timedelta(days=14):
+                 new_vid = Video.objects.create(
+                 title=title,
+                 link=link,
+                 image=image,
+                 source=source,
+                 duration=int(duration),
+                 published= date_object
+                 )
+                 for tag in tags.all():
+                     new_vid.tags.add(tag)
+             else:
+                 print("duplicate or old")
 
          if 'nextPageToken' in search_response:
-           return self.read_playlist(playlist, search_response['nextPageToken'], vids, tags)
-         else:
-           return vids;
-    def read_channel(self, channelName, vids, tags):
+           self.read_playlist(playlist, search_response['nextPageToken'], tags)
+
+    def read_channel(self, channelName, tags):
          DEVELOPER_KEY = settings.GOOGLE_API_KEY
          YOUTUBE_API_SERVICE_NAME = "youtube"
          YOUTUBE_API_VERSION = "v3"
@@ -67,52 +90,50 @@ class Command(BaseCommand):
          ).execute()
          if (len(channel['items'])>0):
              playlist = channel['items'][0]['contentDetails']['relatedPlaylists']['uploads']
-             return self.read_playlist(playlist, None, vids, tags)
-         else:
-             return vids
+             self.read_playlist(playlist, None, tags)
 
 
     def _get_videos(self):
         sources = VideoSource.objects.all()
-        vids = []
+
         for source in sources:
             if source.video_source_type==VideoSourceType.objects.get(video_source_type="youtube_playlist"):
-                vids = self.read_playlist(source.link, None, vids, source.tags)
+                self.read_playlist(source.link, None, source.tags)
             if source.video_source_type==VideoSourceType.objects.get(video_source_type="youtube_channel"):
-                vids = self.read_channel(source.link, vids, source.tags)
-        print(len(vids))
+                self.read_channel(source.link, source.tags)
+
 
         # Video.objects.all().delete()
-        all_videos = Video.objects.all()
-        for vid in vids:
-            title = vid['snippet']['title']
-            link = "https://www.youtube.com/embed/"+vid['id']+"/?autoplay=1&fs=1"
-            if 'standard' in vid['snippet']['thumbnails']:
-                image = vid['snippet']['thumbnails']['standard']['url']
-            else:
-                image = vid['snippet']['thumbnails']['default']['url']
-            source = "Youtube"
-            duration = isodate.parse_duration(vid['contentDetails']['duration']).total_seconds()/60 +1
-            tags = vid['tags']
-            published = vid['snippet']['publishedAt']
-            published = published[:published.find("T")]
-            # 2016-11-22T00:29:37.000Z
-            date_object = datetime.strptime(published, '%Y-%m-%d')
-            print(date_object)
-            print(title + " " + link + " " + image + " " + source + " " + str(duration))
-            if not all_videos.filter(title=title).exists() and date_object>=datetime.now()-timedelta(days=14):
-                new_vid = Video.objects.create(
-                title=title,
-                link=link,
-                image=image,
-                source=source,
-                duration=int(duration),
-                published= date_object
-                )
-                for tag in tags.all():
-                    new_vid.tags.add(tag)
-            else:
-                print("duplicate or old")
+        # all_videos = Video.objects.all()
+        # for vid in vids:
+        #     title = vid['snippet']['title']
+        #     link = "https://www.youtube.com/embed/"+vid['id']+"/?autoplay=1&fs=1"
+        #     if 'standard' in vid['snippet']['thumbnails']:
+        #         image = vid['snippet']['thumbnails']['standard']['url']
+        #     else:
+        #         image = vid['snippet']['thumbnails']['default']['url']
+        #     source = "Youtube"
+        #     duration = isodate.parse_duration(vid['contentDetails']['duration']).total_seconds()/60 +1
+        #     tags = vid['tags']
+        #     published = vid['snippet']['publishedAt']
+        #     published = published[:published.find("T")]
+        #     # 2016-11-22T00:29:37.000Z
+        #     date_object = datetime.strptime(published, '%Y-%m-%d')
+        #     print(date_object)
+        #     print(title + " " + link + " " + image + " " + source + " " + str(duration))
+        #     if not all_videos.filter(title=title).exists() and date_object>=datetime.now()-timedelta(days=14):
+        #         new_vid = Video.objects.create(
+        #         title=title,
+        #         link=link,
+        #         image=image,
+        #         source=source,
+        #         duration=int(duration),
+        #         published= date_object
+        #         )
+        #         for tag in tags.all():
+        #             new_vid.tags.add(tag)
+        #     else:
+        #         print("duplicate or old")
 
 
 
